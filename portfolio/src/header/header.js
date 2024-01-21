3c70b12100ea47bb992d65c50a6c5b59e3f2c324
j <j@j.com>
2024-01-21 18:04:22 +0100

    NavBar items

--- portfolio/src/header/header.js
+++ portfolio/src/header/header.js
@@ -1,9 +1,16 @@
 import './header.css';
 
 function Header(){
-    return (<nav>
-        <span>Link</span>
-    </nav>);
+    return (<header>
+        <nav>
+            <ul>
+                <li><a href="#" className="active">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>);
 }
 
 export default Header;
