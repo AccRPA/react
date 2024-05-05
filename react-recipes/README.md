17dc33d76945b4548b2cd68182966e06ae6e7e35
AccRPA <accrpaw10@gmail.com>
2024-05-05 18:28:09 +0200

    Firebase deploy

---
+++ react-recipes/README.md
@@ -0,0 +1,6 @@
+## REACT RECIPES project
+
+- useEffect, useState, useRef
+- css animations
+
+Deployment in firebase: firebase deploy --only hosting:recipes-search
--- react-recipes/src/app.jsx
+++ react-recipes/src/app.jsx
@@ -2,10 +2,21 @@ import Header from './Header/Header';
 import Recipes from './Recipes/Recipes';
 import RecipeDetails from './RecipeDetails/RecipeDetails';
 import {useState} from 'react';
+import { initializeApp } from "firebase/app";
 
 function App(){
     const [inputSearch, setInputSearch] = useState();
     const [detailId, setDetailId] = useState();
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyA3e9s4lnEsKQveaysfKRkZF-uvKkskNFo",
+        authDomain: "recipes-lookup.firebaseapp.com",
+        projectId: "recipes-lookup",
+        storageBucket: "recipes-lookup.appspot.com",
+        messagingSenderId: "1001400776730",
+        appId: "1:1001400776730:web:9f624bde439f6ccd4b39a3"
+    };
+    const app = initializeApp(firebaseConfig);
 
     function searchChanged(value){
         setInputSearch(value);
