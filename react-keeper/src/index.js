9f29b2b43fdfc34a4124f89c86f5215c961411d6
AccRPA <accrpaw10@gmail.com>
2024-05-11 21:28:46 +0200

    deployed to firebase

--- react-keeper/src/index.js
+++ react-keeper/src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLO6hKYuguGISrX-GRT77XsQrCm-im370",
+  authDomain: "simple-note-keeper.firebaseapp.com",
+  projectId: "simple-note-keeper",
+  storageBucket: "simple-note-keeper.appspot.com",
+  messagingSenderId: "471859818321",
+  appId: "1:471859818321:web:89698d759b73146f5bed70"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
