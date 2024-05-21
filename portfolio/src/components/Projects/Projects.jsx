58f2fd478b51e89bf3b32b9459d8ac4205db0281
AccRPA <accrpaw10@gmail.com>
2024-05-21 22:39:26 +0200

    Adding motion to skills and projects sections when scrolling

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,44 +1,77 @@
-import React from 'react';
 import './Projects.css';
+import React, { useRef, useEffect } from 'react';
 import ProjectItem from '../ProjectItem/ProjectItem';
 import NFL_Logos_Name from '../../assets/apps/NFL_Logos_Name.png';
 import Recipe_Finder from '../../assets/apps/Recipe_Finder.png';
 import Note_Keeper from '../../assets/apps/Note_Keeper.png';
 
-const Projects = () => (
-  <section id="projects">
-      <div className="container">
-          <div className="projects">
-            <div className="title">Projects</div>
-            <div className="items">
-              <ProjectItem 
-                imgUrl={NFL_Logos_Name}
-                url="https://nfl-teams-logos-game.web.app/"
-                githubUrl=""
-                title="NFL Logos Game"
-                description="Built in vanilla Javascript, is an application for knowing all the NFL teams names in the 2023 season"
-              >
-              </ProjectItem>
-              <ProjectItem 
-                imgUrl={Recipe_Finder}
-                url="https://recipes-search.web.app/"
-                githubUrl=""
-                title="Recipe finder"
-                description="Built in React. It uses a public API to get recipes base on one ingredient"
+function Projects(){
+  const projectsRef = useRef();
+
+  useEffect(() => {
+    const timeout = 200;
+    const showElement = (elem, timeout) => {
+        setTimeout(() => {
+            elem.classList.add('popup');                            
+        }, timeout);
+    }
+    const callback = (entries) => {
+        const parent = entries[0];
+        if (parent.isIntersecting){                
+            let elem = parent.target;
+            elem.childNodes.forEach((child, index) => {
+                if (!child.classList.contains('show')){
+                    const delay = timeout * (index + 1);
+                    showElement(child, delay);
+                }
+            });
+        }
+    };
+    const options = {
+        rootMargin: '0px',
+        threshold: 0.3,
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(projectsRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <section id="projects">
+        <div className="container">
+            <div className="projects">
+              <div className="title">Projects</div>
+              <div className="items" ref={projectsRef}>
+                <ProjectItem 
+                  imgUrl={NFL_Logos_Name}
+                  url="https://nfl-teams-logos-game.web.app/"
+                  githubUrl=""
+                  title="NFL Logos Game"
+                  description="Built in vanilla Javascript, is an application for knowing all the NFL teams names in the 2023 season"
                 >
-              </ProjectItem>
-              <ProjectItem 
-                imgUrl={Note_Keeper}
-                url="https://simple-note-keeper.web.app/"
-                githubUrl=""
-                title="Note keeper"
-                description="Built in React. It uses useEffect, useContext and useRef to not only handle adding and deleting notes as well as seeing the details but also to change the theme"
-              >
-              </ProjectItem>
+                </ProjectItem>
+                <ProjectItem 
+                  imgUrl={Recipe_Finder}
+                  url="https://recipes-search.web.app/"
+                  githubUrl=""
+                  title="Recipe finder"
+                  description="Built in React. It uses a public API to get recipes base on one ingredient"
+                  >
+                </ProjectItem>
+                <ProjectItem 
+                  imgUrl={Note_Keeper}
+                  url="https://simple-note-keeper.web.app/"
+                  githubUrl=""
+                  title="Note keeper"
+                  description="Built in React. It uses useEffect, useContext and useRef to not only handle adding and deleting notes as well as seeing the details but also to change the theme"
+                >
+                </ProjectItem>
+              </div>
             </div>
-          </div>
-      </div>
-  </section>
-);
+        </div>
+    </section>
+  );
+}
 
 export default Projects;
--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -9,8 +9,40 @@ import Git from '../../assets/tech-icons/git.png';
 import Sourcetree from '../../assets/tech-icons/sourcetree.png';
 import VSCode from '../../assets/tech-icons/vscode.png';
 import AzureDevOps from '../../assets/tech-icons/azure_devops.png';
+import { useEffect, useRef } from 'react';
 
 function Skills(){
+    const techRef = useRef();
+    
+    useEffect(() => {
+        const timeout = 150;
+        const showElement = (elem, timeout) => {
+            setTimeout(() => {
+                elem.classList.add('show');
+            }, timeout);
+        }
+        const callback = (entries) => {
+            const parent = entries[0];
+            if (parent.isIntersecting){                
+                let elem = parent.target;
+                elem.childNodes.forEach((child, index) => {
+                    if (!child.classList.contains('show')){
+                        const delay = timeout * (index + 1);
+                        showElement(child, delay);
+                    }
+                });
+            }
+        };
+        const options = {
+            rootMargin: '0px',
+            threshold: 0.3,
+        };
+        const observer = new IntersectionObserver(callback, options);
+        observer.observe(techRef.current);
+
+        return () => observer.disconnect();
+    }, []);
+
     return (
     <section id="skills">
         <div className="container">
@@ -19,19 +51,17 @@ function Skills(){
                 <div className="subtitle">
                     Technologies I have been working with recently
                 </div>
-                <div className="tech-stack">
-                {/* this with an array? */}
-                    <img src={Html} alt="Html" title='Html'></img>
-                    <img src={Css} alt="Css" title='Css'></img>
-                    <img src={Js} alt="Js" title='Js'></img>
-                    <img src={ReactIcon} alt="ReactIcon" title='React'></img>
-                    <img src={Bootstrap} alt="Bootstrap" title='Bootstrap'></img>
-                    <img src={Sass} alt="Sass" title='Sass'></img>
-                    <img src={Git} alt="Git" title='Git'></img>
-                    <img src={Sourcetree} alt="Sourcetree" title='Sourcetree'></img>
-                    <img src={VSCode} alt="VSCode" title='Visual Studio code'></img>
-                    <img src={AzureDevOps} alt="AzureDevOps" title='Azure DevOps'></img>
-
+                <div className="tech-stack" id="techRef" ref={techRef}>
+                    <img id="htmlIcon" src={Html} alt="Html" title='Html'></img>
+                    <img id="cssIcon" src={Css} alt="Css" title='Css'></img>
+                    <img id="jsIcon" src={Js} alt="Js" title='Js'></img>
+                    <img id="reactIcon" src={ReactIcon} alt="ReactIcon" title='React'></img>
+                    <img id="btIcon" src={Bootstrap} alt="Bootstrap" title='Bootstrap'></img>
+                    <img id="sassIcon" src={Sass} alt="Sass" title='Sass'></img>
+                    <img id="gitIcon" src={Git} alt="Git" title='Git'></img>
+                    <img id="stIcon" src={Sourcetree} alt="Sourcetree" title='Sourcetree'></img>
+                    <img id="vsIcon" src={VSCode} alt="VSCode" title='Visual Studio code'></img>
+                    <img id="azIcon" src={AzureDevOps} alt="AzureDevOps" title='Azure DevOps'></img>
                 </div>
             </div>
         </div>
