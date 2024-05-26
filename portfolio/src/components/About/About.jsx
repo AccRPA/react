21b2db74554b1611e3530b4e126a9bbc0a87db65
AccRPA <accrpaw10@gmail.com>
2024-05-26 19:21:57 +0200

    Removing question mark in About title. Updated favicon.

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => (
   <section id="about">
       <div className="container">
           <div className="about">
-              <div className="title">About me?</div>
+              <div className="title">About me</div>
               <div className="description item-fade">
                 <p>I am an experienced frontend developer looking for creative projects to bring them alive and make them possible.</p> 
                 <p>Checkout my github with all the javascript exercises and personal projects.</p>
