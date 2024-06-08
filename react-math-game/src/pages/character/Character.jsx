9dec5b3aa0f869d96b049e8809dc82b3e649672b
AccRPA <accrpaw10@gmail.com>
2024-06-08 19:55:07 +0200

    Changed pages animation

--- react-math-game/src/pages/character/Character.jsx
+++ react-math-game/src/pages/character/Character.jsx
@@ -2,8 +2,8 @@ import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
 
 function Character(){
-    return <motion.div initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
+    return <motion.div initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
         className='h-screen bg-slate-100 flex flex-col justify-center align-items-center text-center'
         >
--- react-math-game/src/pages/intro/Intro.jsx
+++ react-math-game/src/pages/intro/Intro.jsx
@@ -2,8 +2,8 @@ import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
 
 function Intro(){
-    return <motion.div initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
+    return <motion.div initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
         className='h-screen bg-slate-100 flex flex-col justify-center align-items-center text-center'>
             <div className='text-sm font-medium text-black sm:text-lg lg:text-xl'>
