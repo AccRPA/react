2826e155c70e4a38bb79bf215c9d0963c880926d
AccRPA <accrpaw10@gmail.com>
2024-06-14 23:08:16 +0200

    Removed unnecessary css and added a test code for testing the storage.

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userStorageKey = 'quizGameUser';
+  const userSaved = window.sessionStorage.getItem(userStorageKey);
+  const [userLoggedIn, setUserLoggedIn] = useState(!!userSaved);
+  
+  function handleClick(){
+    window.sessionStorage.setItem(userStorageKey, 'User');
+    setUserLoggedIn(true);
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  function removeUser(){
+    window.sessionStorage.removeItem(userStorageKey);
+    setUserLoggedIn(false);
+  }
+
+  if(userLoggedIn){
+    // go to finding match and countdown to play
+    return <>
+        <span>The user exists { userLoggedIn }</span>
+        <button onClick={removeUser}>Click to remove it</button>
     </>
-  )
+  }else{
+    // go to login
+    return <button onClick={handleClick}>store user</button>
+  }
 }
 
 export default App
