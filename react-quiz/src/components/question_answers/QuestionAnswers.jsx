1ce5d214c0a34837a817acce0ec7cf0174c0ae92
AccRPA <accrpaw10@gmail.com>
2024-06-30 19:39:31 +0200

    Shuffling the answers

--- react-quiz/src/components/question_answers/QuestionAnswers.jsx
+++ react-quiz/src/components/question_answers/QuestionAnswers.jsx
@@ -1,9 +1,21 @@
 function QuestionAnswers({answers}){
 
-    //answers.shuffle();
+    function shuffleArray(answers){
+        const temp = [...answers];
+        for(let i = 0; i < temp.length; i++){
+            const random = Math.floor(Math.random() * temp.length);
+            [temp[i], temp[random]] = [temp[random], temp[i]];
+        }
+        return temp;
+    }
+
+
+    function handleClick(){
+        // event to send the selected answer
+    }
     
     return <div>
-            {answers.map((item, index) => <p key={index}>{item}</p>)}
+            {shuffleArray(answers).map((item, index) => <span onClick={handleClick} key={index}>{item}</span>)}
         </div>;
 }
 
