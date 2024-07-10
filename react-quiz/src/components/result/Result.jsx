fa724633d3c47b95e04c7133df2d4a8a490cc148
AccRPA <accrpaw10@gmail.com>
2024-07-10 22:47:14 +0200

    Layout for Result screen.

--- react-quiz/src/components/result/Result.jsx
+++ react-quiz/src/components/result/Result.jsx
@@ -1,21 +1,22 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GameContext } from '../../core/GameContext';
-import DisconnectButton from '../disconnect-button/DisconnectButton';
 import User from '../user/User';
-import LeaveRoom from '../leave-room/LeaveRoom';
-import Score from '../score/Score';
 import { socket } from '../../core/socket';
 import Header from '../header/Header';
+import { Utils } from '../../common/Utils';
 
 function Result(){
-    const gameContext = useContext(GameContext);   
+    const gameContext = useContext(GameContext);
+    const [gameResult, setGameResult] = useState({});
+    const [page, setPage] = useState(0);
 
     useEffect(() => {
         function onGameResult({questions, answers, partnerAnswers}){
-            console.log(questions);
-            console.log(answers);
-            console.log(partnerAnswers);
-            console.log('displying game results');
+            setGameResult({
+                questions,
+                answers,
+                partnerAnswers
+            })
         }
 
         socket.on('game_result_response', onGameResult);
@@ -25,26 +26,71 @@ function Result(){
         };
     });
 
-    return <div>
+    function decreasePage(){
+        if (page > 0){
+            setPage(previous => --previous);
+        }
+    }
+    
+    function increasePage(){
+        if (page < gameResult?.questions?.length - 1){
+            setPage(previous => ++previous);
+        }
+    }
+
+    return <div className="root-container">
             <Header showConnections={false} 
                 showTitle={true} 
                 showLeaveButton={true} 
                 showDisconnectButton={true}>                    
             </Header>
-            <p>Game finished! Result:</p>
-            <User 
-                data={gameContext.gameData.userData} 
-                score={gameContext.gameData.game.score} 
-                showScore={true} 
-                showYou={true}>                    
-            </User>
-            {!!gameContext.gameData.partnerData &&  
-                <User 
-                    data={gameContext.gameData.partnerData}
-                    score={gameContext.gameData.game.partnerScore} 
-                    showScore={true}>
-                </User>
-            }
+            <div className="game-container">
+                <div className='users'>
+                    <div>
+                        <User 
+                            data={gameContext.gameData.userData} 
+                            score={gameContext.gameData.game.score} 
+                            showScore={true} 
+                            showYou={true}>                    
+                        </User>
+                    </div>
+                    <div>
+                        {!!gameContext.gameData.partnerData &&  
+                            <User 
+                            data={gameContext.gameData.partnerData}
+                            score={gameContext.gameData.game.partnerScore} 
+                            showScore={true}
+                            switchOrder={true}>
+                            </User>
+                        }
+                    </div>
+                </div>
+
+                <div className="game-content">
+                    <div className='card-content'>
+                        <p>Game finished! Result:</p>
+                        {!!gameResult?.questions && 
+                            <>
+                                <div>
+                                    {gameResult.questions[page].number}
+                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].category)}
+                                    {Utils.decodeHtmlCharactersg(gameResult.questions[page].question)}
+                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].correctAnswer)}
+                                </div>
+                                <div>
+                                    {!!gameResult?.answers && 
+                                        <div>You: {Utils.decodeHtmlCharacters(gameResult.answers[page].answer)} {gameResult.answers[page].valid}</div>}
+                                    {!!gameContext.gameData.partnerData && !!gameResult.partnerAnswers &&
+                                        <div>Partner: {Utils.decodeHtmlCharacters(gameResult.partnerAnswers[page].answer)} {gameResult.partnerAnswers[page].valid}</div>
+                                    }
+                                </div>
+                                <button disabled={page == 0 && 'disabled'} onClick={decreasePage}>Prev</button>
+                                <button disabled={page === gameResult.questions.length - 1  && 'disabled'} onClick={increasePage}>Next</button>
+                            </>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>;
 }
 
