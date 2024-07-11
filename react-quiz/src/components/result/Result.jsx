434f999bfbb251d25abb0f876699c2b3f3dac19f
AccRPA <accrpaw10@gmail.com>
2024-07-11 22:27:01 +0200

    Styles for the result screen. Responsive design adjusted.

--- react-quiz/src/components/result/Result.jsx
+++ react-quiz/src/components/result/Result.jsx
@@ -4,11 +4,16 @@ import User from '../user/User';
 import { socket } from '../../core/socket';
 import Header from '../header/Header';
 import { Utils } from '../../common/Utils';
+import { EmojiFrown, EmojiGrin, EmojiSmile, EmojiSunglasses, EmojiTear } from 'react-bootstrap-icons';
 
 function Result(){
     const gameContext = useContext(GameContext);
     const [gameResult, setGameResult] = useState({});
     const [page, setPage] = useState(0);
+    const result = 
+        !gameContext.gameData.partnerData ?
+        0 :
+        gameContext.gameData.game.score - gameContext.gameData.game.partnerScore;
 
     useEffect(() => {
         function onGameResult({questions, answers, partnerAnswers}){
@@ -68,25 +73,71 @@ function Result(){
 
                 <div className="game-content">
                     <div className='card-content'>
-                        <p>Game finished! Result:</p>
+                        <span className="highlight result-highlight">
+                            <span>Game finished!</span>                        
+                            {result > 0 && 
+                            <span className='result-highlight'>
+                                <span>You've won!</span>
+                                <EmojiGrin></EmojiGrin>
+                            </span> 
+                            }
+                            {result < 0 && 
+                            <span className='result-highlight'>
+                                <span>You've lost</span>
+                                <EmojiTear></EmojiTear>
+                            </span> 
+                            }
+                            {!!gameContext.gameData.partnerData && result == 0 && 
+                            <span className='result-highlight'>
+                                <span>It's a tie</span>
+                                <EmojiSunglasses></EmojiSunglasses>
+                            </span> 
+                            }
+                        </span>
                         {!!gameResult?.questions && 
-                            <>
-                                <div>
-                                    {gameResult.questions[page].number}
-                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].category)}
-                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].question)}
-                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].correctAnswer)}
+                            <div className='question-container'>
+                                <div className='question-info'>
+                                    <p>Question {gameResult.questions[page].number}/{gameResult.questions.length}</p>
+                                    <p>{Utils.decodeHtmlCharacters(gameResult.questions[page].category)}</p>
                                 </div>
-                                <div>
+                                <p className='question'>
+                                    {Utils.decodeHtmlCharacters(gameResult.questions[page].question)}
+                                </p>
+                                <div className='result-container'>
                                     {!!gameResult?.answers && 
-                                        <div>You: {Utils.decodeHtmlCharacters(gameResult.answers[page].answer)} {gameResult.answers[page].valid}</div>}
+                                        <div className='result-item'>
+                                            <div>{gameContext.gameData.userData.name}(You)</div> 
+                                            <div className='result-player'>
+                                                {Utils.decodeHtmlCharacters(gameResult.answers[page].answer) || '-' } 
+                                                {gameResult.answers[page].valid ? 
+                                                    <EmojiSmile className='correct'></EmojiSmile> : 
+                                                    <EmojiFrown className='wrong'></EmojiFrown> }
+                                            </div>      
+                                        </div>
+                                    }
+                                    <div className='result-item'>
+                                        <div className='result-answer'>Answer</div>
+                                        <div>
+                                            {Utils.decodeHtmlCharacters(gameResult.questions[page].correctAnswer)}
+                                        </div>
+                                    </div>
                                     {!!gameContext.gameData.partnerData && !!gameResult.partnerAnswers &&
-                                        <div>Partner: {Utils.decodeHtmlCharacters(gameResult.partnerAnswers[page].answer)} {gameResult.partnerAnswers[page].valid}</div>
+                                        <div className='result-item'>
+                                            <div>{gameContext.gameData.partnerData.name}</div>
+                                            <div className='result-player'>
+                                                {Utils.decodeHtmlCharacters(gameResult.partnerAnswers[page].answer) || '-'} 
+                                                {gameResult.partnerAnswers[page].valid ? 
+                                                    <EmojiSmile className='correct'></EmojiSmile> : 
+                                                    <EmojiFrown className='wrong'></EmojiFrown>}
+                                            </div>
+                                        </div>
                                     }
                                 </div>
-                                <button disabled={page == 0 && 'disabled'} onClick={decreasePage}>Prev</button>
-                                <button disabled={page === gameResult.questions.length - 1  && 'disabled'} onClick={increasePage}>Next</button>
-                            </>
+                                <div className="button-group-result">
+                                    <button className="answer-button" disabled={page == 0 && 'disabled'} onClick={decreasePage}>Prev</button>
+                                    <button className="answer-button" disabled={page === gameResult.questions.length - 1  && 'disabled'} onClick={increasePage}>Next</button>
+                                </div>
+                            </div>
                         }
                     </div>
                 </div>
