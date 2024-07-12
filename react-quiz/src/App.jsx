eeef13c0014285f3728ab08ea402c8759744ea66
AccRPA <accrpaw10@gmail.com>
2024-07-12 14:46:59 +0200

    Error boundaries added

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css'
-import { useState } from 'react'
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
+import { ErrorBoundary } from 'react-error-boundary'
 import { GameContext } from './core/GameContext';
 import Root from './components/root/Root';
+import ErrorPage from './components/error/Error-page';
+import { socket } from './core/socket';
+import { GameData } from './core/GameData';
 
 function App() {
   const gameContext = useContext(GameContext);
   const [gameData, setGameData] = useState(gameContext.gameData);  
   const gameContextState = { gameData, setGameData };
 
+  const deleteUser = (error, info) => {
+    console.log(`Error: ${error}. ${info}`);
+    socket.disconnect();
+    gameContext.setGameData(new GameData());
+  };
+
   return <GameContext.Provider value={gameContextState}>
-      <Root></Root>
+      <ErrorBoundary
+        FallbackComponent={ErrorPage}
+        onError={deleteUser}
+      >
+        <Root></Root>
+
+      </ErrorBoundary>
     </GameContext.Provider>
 }
 
--- react-quiz/src/components/error/Error-page.jsx
+++ react-quiz/src/components/error/Error-page.jsx
@@ -1,17 +1,25 @@
-import { useRouteError } from "react-router-dom";
 import './Error-page.css';
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+
+    function goPlay(){
+        location.href = '';
+    }
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-            <i>{error.statusText || error.message}</i>
-            </p>
+        <div className="error-container">
+            <div>Quiz Game!</div>
+            <div className="game-container">
+                <div className="game-content">
+                    <div className='card-content'>
+                        <div id="error-page">
+                            <h1>Oops!</h1>
+                            <p>Sorry, an unexpected error has occurred.</p>
+                            <button className="button primary" onClick={goPlay}>Go play</button>
+                        </div>
+                    </div>
+                </div>                
+            </div>
         </div>
     );
 }
