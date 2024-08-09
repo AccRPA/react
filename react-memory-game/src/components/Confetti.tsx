16fdffa677b54c42bdc0a250fec860e61834fd52
AccRPA <accrpaw10@gmail.com>
2024-08-09 15:25:04 +0200

    Responsive design and favicon

--- react-memory-game/src/components/Confetti.tsx
+++ react-memory-game/src/components/Confetti.tsx
@@ -1,5 +1,7 @@
+import { Options } from "canvas-confetti";
 import { useState } from "react";
 import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import Pride from "react-canvas-confetti/dist/presets/pride";
 import { TConductorInstance } from "react-canvas-confetti/dist/types";
 import { TCanvasConfettiInstance } from "react-canvas-confetti/dist/types/normalization";
 
@@ -9,9 +11,23 @@ interface Props {
 
 export default ({showConfetti}: Props) => {
     const [conductor, setConductor] = useState<TConductorInstance>();
+    const colors: string[] = ['#FFD700', '#1d93ec', '#FF7373', '#40E0D0'];
 
     const handleOnInit = (params: {confetti: TCanvasConfettiInstance, conductor: TConductorInstance}) => {
         setConductor(params.conductor);
+    }
+    const handleOptionsFireworks = (options: Options): Options => {
+        return {
+            ...options,
+            colors: colors
+        };
+    }
+    const handleOptionsPride = (options: Options): Options => {
+        return {
+            ...options,
+            particleCount: 100,
+            colors: colors
+        };
     }
 
     if (showConfetti){
@@ -19,7 +35,8 @@ export default ({showConfetti}: Props) => {
     }
 
     return (<>
-        <Fireworks onInit={handleOnInit}/>
+        {/* <Fireworks onInit={handleOnInit} decorateOptions={handleOptionsFireworks}/> */}
+        <Pride onInit={handleOnInit} decorateOptions={handleOptionsPride}></Pride>
     </>
 )
 }
