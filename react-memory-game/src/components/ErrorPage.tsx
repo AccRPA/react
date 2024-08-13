6ed9370195a41216f675c4c7797e22c4e92f0e6f
AccRPA <accrpaw10@gmail.com>
2024-08-13 19:09:22 +0200

    Error page styled.

--- react-memory-game/src/components/ErrorPage.tsx
+++ react-memory-game/src/components/ErrorPage.tsx
@@ -1,9 +1,38 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 interface Props {
     code: string
 }
 
 function ErrorPage({code}: Props){
-    return <div>Error {code}</div>;
+    console.log(`Error code ${code}`);
+    
+    useEffect(() => {
+        document.getElementById('errorCards')?.classList.add('animate');
+    });
+    
+    return (
+        <>
+            <div className="error-header">
+                <h1>Oops...</h1>
+                <h3>We didn't find what you were looking for</h3>
+
+            </div>
+            <div className="error-cards" id="errorCards">                
+                <div className="card" id="card1">
+                    <div className="front">4</div>
+                </div>
+                <div className="card" id="card2">
+                    <div className="front">{String.fromCodePoint(0x1F622)}</div>
+                </div>
+                <div className="card" id="card3">
+                    <div className="front">4</div>
+                </div>                
+            </div>
+            <Link className="link" to ="/">Go Home</Link>
+        </>
+    );
 }
 
 export default ErrorPage;
