a566175d8ab494f82c342ab5ebea039e154f732d
AccRPA <accrpaw10@gmail.com>
2024-08-16 17:18:54 +0200

    Added firebase

--- react-memory-game/src/main.tsx
+++ react-memory-game/src/main.tsx
@@ -4,6 +4,22 @@ import RoutesApp from './components/Routes.tsx';
 import { ThemeProvider } from '@mui/material';
 import { GameTheme } from './theme/GameTheme.ts';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-HNxLm4Hu2BdRA7PmVukuR5-kIcFS5XA",
+  authDomain: "memory-card-game-react.firebaseapp.com",
+  projectId: "memory-card-game-react",
+  storageBucket: "memory-card-game-react.appspot.com",
+  messagingSenderId: "48514024921",
+  appId: "1:48514024921:web:9e15543a50ce364f8a45c8"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={GameTheme}>
